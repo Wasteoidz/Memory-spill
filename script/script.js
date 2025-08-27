85af1e4b197f543c2c0210edd49f90578c6ffc8c
Wasteoidz <Rolf91@live.com>
2025-08-27 13:57:24 +0200

    ny knapp
    laget drawboard funksjon

--- script/script.js
+++ script/script.js
@@ -5,33 +5,23 @@ let cards = 0;
 updateVeiw();
 function updateVeiw() {
     app.innerHTML = /*HTML*/`
-    <h1>Memory game!</h1>
+    <h1>Memory game!</h1> 
     <div id="boardGame">
-    <div onclick="game()" class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    <div class="cards"></div>
-    </div>
+    </div> <br>
+    <button class="knapp" onclick=drawBoard()>Start Spill</button>
     `;
-};
+}
 //Controller
 function game(){
     
-};
+}
+
+function drawBoard() {
+    return document.getElementById('boardGame').innerHTML = `<div class="cards"></div>`.repeat(16);
+        updateVeiw();
+}
 
 function show() {
     document.getElementById().style.display = "block";        
 
-};
+}
