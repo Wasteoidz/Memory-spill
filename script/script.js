83c75be21730c925560939b9ffca57e607ff3ff4
Benjamin Mehmi <benjaminmehmi@hotmail.com>
2025-08-27 18:45:06 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,27 +1,91 @@
 //Model
 const app = document.getElementById('spill');
-let cards = 0;
-//Veiw
-updateVeiw();
-function updateVeiw() {
+
+
+//View
+updateView()
+function updateView() {
     app.innerHTML = /*HTML*/`
-    <h1>Memory game!</h1> 
-    <div id="boardGame">
-    </div> <br>
-    <button class="knapp" onclick=drawBoard()>Start Spill</button>
+    
+    <h1>Memory game!</h1>
+    
+    <section class="memory-game">
+
+        <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
+        </div>
+
+         <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
+        </div>
+
+           <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
+        </div>
+
+         <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
+        </div>
+
+           <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
+        </div>
+
+         <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
+        </div>
+
+           <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
+        </div>
+
+         <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
+        </div>
+           <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
+        </div>
+
+         <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
+        </div>
+
+           <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
+        </div>
+
+         <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
+        </div>
+
+    <section>
     `;
-}
+
+    const cards = document.querySelectorAll('.memory-card'); /* henter css klassen .memory-card og deklarerer den som cards */   
+
+    cards.forEach(card => card.addEventListener('click', flipCard)); /* legger til en eventlistener til hvert kort hvor vi har funksjonen flipCard() */  
+    
+}    
+   
+
 //Controller
 function game(){
     
-}
+};
 
-function drawBoard() {
-    return document.getElementById('boardGame').innerHTML = `<div class="cards"></div>`.repeat(16);
-        updateVeiw();
-}
-
-function show() {
-    document.getElementById().style.display = "block";        
-
-}
+function flipCard() { /* flipper kortene */
+    this.classList.toggle('flip');
+    console.log('click');
+}  
