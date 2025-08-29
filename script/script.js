ac2f1342e5d55f77232709cf6a5c4070871243d5
andwar92 <andwar1992@gmail.com>
2025-08-29 10:00:19 +0200

    MemoGame
    Lagt til bilder

--- script/script.js
+++ script/script.js
@@ -6,85 +6,76 @@ const app = document.getElementById('spill');
 updateView()
 function updateView() {
     app.innerHTML = /*HTML*/`
-    <h3 class="h3">100% Win Rate!</h3>
-    <h1 class="header">Memory game for Dummies!</h1>
+    <h1 class="header">&#128040;Memory game&#128040;</h1>
     
     <section class="memory-game">
 
+        <div id="1"class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
+        </div>
+        <div id="1" class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
+        </div>
+        <div id="2" class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20090210-668MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
+        </div>
+        <div id="2"class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20090210-668MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
+        </div>
         <div class="memory-card">
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-                <img class="bakside">
+                <img class="forside" src="css/pictures/NZP-20100323-121MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
+        </div>
+        <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20100323-121MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
+        </div>
+        <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20111005-064MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
+        </div>
+        <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20111005-064MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
+        </div>
+        <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20111005-157MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
+        </div>
+        <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20111005-157MM_thumb.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
+        </div>
+        <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20190904_0434SP_thumb.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
+        </div>
+        <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20190904_0434SP_thumb.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
+        </div>
+        <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20191024_001RP_thumb.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
+        </div>
+        <div class="memory-card">
+                <img class="forside" src="css/pictures/NZP-20191024_001RP_thumb.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
+        </div>
+        <div class="memory-card">
+                <img class="forside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
+        </div>
+        <div class="memory-card">
+                <img class="forside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
+                <img class="bakside" src="css/pictures/question-mark.png">
         </div>
 
-         <div class="memory-card">
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-                <img class="bakside" >
-        </div>
-
-           <div class="memory-card">
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-              
-        </div>
-
-         <div class="memory-card">
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-              
-        </div>
-
-           <div class="memory-card">
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-              
-        </div>
-
-         <div class="memory-card">
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-              
-        </div>
-
-           <div class="memory-card">
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-              
-        </div>
-
-         <div class="memory-card">
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-              
-        </div>
-           <div class="memory-card">
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-              
-        </div>
-
-         <div class="memory-card">
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-              
-        </div>
-
-           <div class="memory-card">2
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-              
-        </div>
-
-         <div class="memory-card">
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-              
-        </div>
-         <div class="memory-card">
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-              
-        </div>
-         <div class="memory-card">
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-              
-        </div>
-         <div class="memory-card">
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-              
-        </div>
-         <div class="memory-card">
-                <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
-              
-        </div>
+        
         
         <section>
     `;
@@ -96,13 +87,7 @@ function updateView() {
     let randomPos = Math.floor(Math.random() * cards.length);
     card.style.order = randomPos;});
 }    
-   
-
 //Controller
-function game(){
-    
-};
-
 function flipCard() { /* flipper kortene */
     this.classList.toggle('flip');
     console.log('click');
