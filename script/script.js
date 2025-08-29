01ba6e422510ad2f507702155188426f212db9d1
Wasteoidz <Rolf91@live.com>
2025-08-29 11:59:32 +0200

    småfix

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 //Model
 const app = document.getElementById('spill');
-const cards = document.querySelectorAll('.memory-card'); /* henter css klassen .memory-card og deklarerer den som cards */
+
 
 //View
 updateView()
@@ -10,67 +10,67 @@ function updateView() {
     
     <section class="memory-game">
 
-        <div data-id="1" class="memory-card">
+        <div id="1" class="memory-card">
                 <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
-        <div data-id="1" class="memory-card">
+        <div id="1" class="memory-card">
                 <img class="forside" src="css/pictures/NZP-20071211-117MM_thumb.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
-        <div data-id="2" class="memory-card">
+        <div id="2" class="memory-card">
                 <img class="forside" src="css/pictures/NZP-20090210-668MM_thumb.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
-        <div data-id="2" class="memory-card">
+        <div id="2" class="memory-card">
                 <img class="forside" src="css/pictures/NZP-20090210-668MM_thumb.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
-        <div data-id="3" class="memory-card">
+        <div id="3" class="memory-card">
                 <img class="forside" src="css/pictures/NZP-20100323-121MM_thumb.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
-        <div data-id="3" class="memory-card">
+        <div id="3" class="memory-card">
                 <img class="forside" src="css/pictures/NZP-20100323-121MM_thumb.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
-        <div data-id="4" class="memory-card">
+        <div id="4" class="memory-card">
                 <img class="forside" src="css/pictures/NZP-20111005-064MM_thumb.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
-        <div data-id="4" class="memory-card">
+        <div id="4" class="memory-card">
                 <img class="forside" src="css/pictures/NZP-20111005-064MM_thumb.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
-        <div data-id="5" class="memory-card">
+        <div id="5" class="memory-card">
                 <img class="forside" src="css/pictures/NZP-20111005-157MM_thumb.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
-        <div data-id="5" class="memory-card">
+        <div id="5" class="memory-card">
                 <img class="forside" src="css/pictures/NZP-20111005-157MM_thumb.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
-        <div data-id="6" class="memory-card">
+        <div id="6" class="memory-card">
                 <img class="forside" src="css/pictures/NZP-20190904_0434SP_thumb.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
-        <div data-id="6" class="memory-card">
+        <div id="6" class="memory-card">
                 <img class="forside" src="css/pictures/NZP-20190904_0434SP_thumb.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
-        <div data-id="7" class="memory-card">
+        <div id="7" class="memory-card">
                 <img class="forside" src="css/pictures/NZP-20191024_001RP_thumb.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
-        <div data-id="7" class="memory-card">
+        <div id="7" class="memory-card">
                 <img class="forside" src="css/pictures/NZP-20191024_001RP_thumb.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
-        <div data-id="8" class="memory-card">
+        <div id="0" class="memory-card">
                 <img class="forside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
-        <div data-id="8" class="memory-card">
+        <div id="0" class="memory-card">
                 <img class="forside" src="css/pictures/cartoon-safari-clip-art-orangutan.jpg">
                 <img class="bakside" src="css/pictures/question-mark.png">
         </div>
@@ -80,36 +80,47 @@ function updateView() {
         <section>
     `;
 
-
+        const cards = document.querySelectorAll('.memory-card'); /* henter css klassen .memory-card og deklarerer den som cards */
         cards.forEach(card => card.addEventListener('click', flipCard,)); /* legger til en eventlistener til hvert kort hvor vi har funksjonen flipCard() */
         cards.forEach(card => {
-                let randomPos = Math.floor(Math.random() * cards.length);
+                const randomPos = Math.floor(Math.random() * cards.length);
                 card.style.order = randomPos;
         });
 }
 //Controller
 
-let flippedCard = false;
-let firstCard, secondCard;
+// let flippedCard = false;
 
-function flipCard() { /* flipper kortene */
-        this.classList.toggle('flip');
-        if(!flippedCard === !flippedCard){
-        flippedCard = true;
-        firstCard = this;     
+// function flipCard(firstCard, secondCard) { /* flipper kortene */
+//         this.classList.toggle('flip');
+//         if(!flippedCard){
+//         flippedCard = true;
+//         firstCard = this;     
 
-        console.log(flippedCard, firstCard) 
-        }
+//         console.log(flippedCard, firstCard) 
+//         }
         
-        else{
-                flippedCard = false;
-                secondCard = this;
-        }
+//         else{
+//                 flippedCard = false;
+//                 secondCard = this;
+//         }
 
-       /* if(firstCard.dataset.id === secondCard.dataset.id){
-          firstCard.removeEventListener('click', flipCard,);      
-          secondCard.removeEventListener('click', flipCard,);      
-        } */
+//         checkIfMatch()
         
+// function checkIfMatch(){
+
+
+//         if(firstCard.dataset.framework === secondCard.dataset.framework){
+//           firstCard.removeEventListener('click', flipCard,);      
+//           secondCard.removeEventListener('click', flipCard,);      
+//         } 
         
-}; 
+// }        
+        
+// }; 
+
+
+function flipCard() {  /* flipper kortene */
+    this.classList.toggle('flip');
+    console.log('click'); 
+    }
